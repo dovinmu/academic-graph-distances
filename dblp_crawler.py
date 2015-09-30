7de19051845f30208daba144480015c892179d6b
rowan <dovinmu@gmail.com>
2015-09-30 12:22:37 -0700

    Crawling and matching work but pathfinding on the graph is not necessarily the shortest'

---
+++ dblp_crawler.py
@@ -0,0 +1,53 @@
+import time
+import random
+from bs4 import BeautifulSoup
+import requests
+
+BASE_URL = 'http://dblp.uni-trier.de/pers/xc/'
+start = 0
+iters = 4
+names = {}
+graph = {}
+
+with open('round_' + str(start)) as f:
+    for line in f:
+        if line.strip() != '':
+            line = line.split(';')
+            names[line[1].strip()] = line[0].strip()
+            graph[line[0].strip()] = set()
+            
+processing_nodes = [name for name in names.keys()]
+
+for n in range(start+1,iters+1):
+    newly_added = []
+    i = 0
+    for name_url in processing_nodes:
+        author_name = names[name_url]      
+        r = requests.get(BASE_URL + name_url)
+        soup = BeautifulSoup(r.text)
+        current_length = len(newly_added)
+        for coauthor in soup.find_all('author'):
+            coauthor_url = coauthor.get('urlpt')
+            coauthor_name = coauthor.text
+            if coauthor_url not in names:
+                names[coauthor_url] = coauthor_name
+                graph[coauthor_name] = set([author_name])
+                newly_added.append(coauthor_url)
+            graph[author_name].add(coauthor_name)
+        i += 1
+        print('%d/%d. Added %d more names from author %s' % (i,len(processing_nodes),len(newly_added) - current_length, names[name_url]))
+        time.sleep(2)
+    
+    with open('round_'+str(n),'w') as f:
+        for author in newly_added:
+            f.write(names[author] + ' ; ' + author + '\n')
+    with open('author_graph_round_'+str(n),'w') as f:
+        for author in graph.keys():
+            s = author + ':'
+            for coauthor in graph[author]:
+                s += coauthor + ','
+            f.write(s + '\n')
+    
+    print('done with round %d, size of names: %d' % (n,len(names)))
+    processing_nodes = [name for name in newly_added]
+
